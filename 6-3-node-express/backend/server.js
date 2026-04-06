9145ed87f966e16ae0280088bc6d1952b1b09bed
Naba-Alali <S202268320@kfupm.edu.sa>
2026-04-06 11:33:50 +0300

    Todo6

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,8 +1,10 @@
 // TODO 1: Import and Create express app instance
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
+
 
 // TODO 1: Define server port
 const PORT = process.env.PORT || 3000;
@@ -11,12 +13,19 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
-
+app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
+app.get("/", (req, res) => {
+  res.send("Welcome to the Quotes API!");
+});
 
 
 // TODO 6.2: Create "/api/quote" route
+app.get("/api/quote", (req, res) => {
+ const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 
 // TODO 7: Start server using app.listen
